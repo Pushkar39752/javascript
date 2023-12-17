806b6ed23494ef85ffdb32f9be77155e2ffd3eca
Pushkar39752 <139541008+Pushkar39752@users.noreply.github.com>
2023-12-17 14:39:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountid=12345
+
+    let accountemail="pushkar@hero.com"
+    var accpass="11111"
+    acccity="jaipur"
+    let acccement;
+    // dont use var in javascript because of the issue of block scope and functional scope
+//accountid=1111
+//console.log(accountid)
+console.table([accountemail,accpass,acccity,acccement])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let age =18
+let isloggedin=false
+let state;
+
+//number 
+//bigint
+//string=""
+//boolean true,false
+//undefined
+//symbol=uniqueness
+console.log(typeof null)
